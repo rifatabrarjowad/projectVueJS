ca49cbcc7038c17ab2d96bfdc68eea1cc4b47b51
RIFAT ABRAR JOWAD <rajdiprifat2004@gmail.com>
2021-08-03 22:17:07 +0600

    singup from

--- resto-project/src/App.vue
+++ resto-project/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <SingUp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SingUp from "./components/SingUp.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SingUp,
+  },
+};
 </script>
 
 <style>
---
+++ resto-project/src/components/SingUp.vue
@@ -0,0 +1,38 @@
+<template>
+  <img class="logo" alt="logo" src="../assets/resto.png" />
+  <div><h1>Sing Up!</h1></div>
+  <div class="register">
+    <input type="text" placeholder="Enter Name" name="" id="" />
+    <input type="email" placeholder="Enter Mail" name="" id="" />
+    <input type="password" placeholder="Enter Password" name="" id="" />
+    <button>Sing Up</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "SingUp",
+};
+</script>
+<style >
+.logo {
+  width: 100px;
+}
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+}
+.register button {
+  width: 300px;
+  height: 40px;
+  border: 1px solid skyblue;
+  background: skyblue;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
